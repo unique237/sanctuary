cb8f3b1ee9d344fd9e11fb91d620dab30fdedfff
unique237 <martin44line@gmail.com>
2024-11-25 05:12:38 +0100

    Update

--- db.sql
+++ db.sql
@@ -4,6 +4,7 @@ CREATE TABLE admin(
 	lname VARCHAR(50),
 	email VARCHAR(50),
 	password VARCHAR(50),
+	rank VARCHAR(25),
 	acct_creation_date TIMESTAMP DEFAULT NOW(),
 );
 
@@ -21,11 +22,25 @@ CREATE TABLE predication(
 
 CREATE TABLE article(
 	articleId serial,
+	image_link VARCHAR(150),
 	title VARCHAR(50),
 	CATEGORY VARCHAR(50),
 	description VARCHAR(255),
 	content TEXT,
 	publish_date TIMESTAMP DEFAULT NOW(),
+	adminId INT,
+	FOREIGN KEY adminId REFERENCES admin(adminId)
+);
+
+CREATE TABLE event(
+	eventId serial,
+	event_title VARCHAR(50),
+	image_link VARCHAR(150),
+	event_description TEXT,
+	start_date DATE,
+	finish_date DATE,
+	form_link VARCHAR(75)
+	post_date TIMESTAMP DEFAULT NOW(),
 	adminId INT,
 	FOREIGN KEY adminId REFERENCES admin(adminId)
 );
